74a0048adddb99d5d2121419801aed95bb43f25a
emidio@morgia.info <emidio@morgia.info>
2024-02-24 16:32:44 +0100

    test

--- src/front-end/app/src/App.tsx
+++ src/front-end/app/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Home component
+          Home component a
         </a>
       </header>
     </div>
