ec9fe2bbe81170cb61a0f72e8bff6ae4c40b5a78
emidio@morgia.info <emidio@morgia.info>
2024-02-24 15:40:47 +0100

    First Commit Bff

---
+++ src/services/bff/src/test/java/info/morgia/timetide/bff/BffApplicationTests.java
@@ -0,0 +1,13 @@
+package info.morgia.timetide.bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
